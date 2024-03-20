93930e295f97ab98fadab294a5b0975726f4bc09
Julia Koma <juliackoma@gmail.com>
2024-03-20 16:19:25 +0100

    started javaFx project

---
+++ game/src/main/java/com/example/GUI.java
@@ -0,0 +1,22 @@
+package com.example;
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class GUI extends Application {
+    
+    @Override
+    public void start(Stage stage) {
+        Group root = new Group();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+
+
+        stage.show();
+    }
+    public static void main(String[] args){
+        launch(args);
+    }
+
+}

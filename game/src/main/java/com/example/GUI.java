accd30a1e173d9193aac40e20ca4bc135e53b2b0
Julia Koma <juliackoma@gmail.com>
2024-03-20 16:31:09 +0100

    testing

--- game/src/main/java/com/example/GUI.java
+++ game/src/main/java/com/example/GUI.java
@@ -12,7 +12,7 @@ public class GUI extends Application {
         Scene scene = new Scene(root);
         stage.setScene(scene);
 
-        System.out.println("testing github");
+        System.out.println("hi github");
 
         stage.show();
     }

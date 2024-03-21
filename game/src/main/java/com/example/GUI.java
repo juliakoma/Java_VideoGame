4fdfcd772fbcbc0557b1474c7d581a23d50d0bbd
Julia Koma <juliackoma@gmail.com>
2024-03-21 20:42:45 +0100

    more tests

--- game/src/main/java/com/example/GUI.java
+++ game/src/main/java/com/example/GUI.java
@@ -1,22 +1,50 @@
 package com.example;
+
+import javafx.util.Duration;
+import javafx.animation.Interpolator;
+import javafx.animation.RotateTransition;
+import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.transform.Rotate;
 import javafx.stage.Stage;
 
 public class GUI extends Application {
-    
+
     @Override
     public void start(Stage stage) {
         Group root = new Group();
-        Scene scene = new Scene(root);
+        Scene scene = new Scene(root, 800, 800);
         stage.setScene(scene);
 
-        System.out.println("hi github");
+        Image layer2 = new Image("file:background_layer2.png");
+        Image layer3 = new Image("file:background_layer3.png");
 
-        stage.show();
+        //Image layer1 = new Image("file:images/background_layer1.png");
+        Image layer1 = new Image("file:images/background_0");
+
+        ImageView layer1View = new ImageView(layer1);
+        layer1View.setFitWidth(510); // Set the width of the ImageView
+        layer1View.setFitHeight(510); // Set the height of the ImageView
+        
+        root.getChildren().add(layer1View); // Add the ImageView to the Group
+        
+        stage.setScene(scene); // Set the Scene to the Stage
+        
+        stage.show(); // Show the Stage
+        
+        
+
     }
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         launch(args);
     }
 
---
+++ game/src/main/java/com/example/GameScreen.java
@@ -0,0 +1,10 @@
+package com.example;
+
+import javax.swing.JFrame;
+
+public class GameScreen extends JFrame{
+    public GameScreen(){
+        setSize(400,400);
+
+    }
+}
